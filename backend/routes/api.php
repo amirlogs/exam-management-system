5a49290b6caa618c0ec4cb079eee4de20b1ca19f
miraz <amiraliyimer@gmail.com>
2026-07-12 22:47:51 +0300

    adding trait for api resonse and login route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,11 +1,11 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
+Route::get("/user", function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
+})->middleware("auth:sanctum");
 
-
-Route::post('auth/login', []);
+Route::post("/auth/login", [AuthController::class, "login"]);
