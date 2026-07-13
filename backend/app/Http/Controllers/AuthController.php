3f2761fc6528c9ea40dce2f1a93a40f30c35aaa6
miraz <amiraliyimer@gmail.com>
2026-07-13 23:11:04 +0300

    all auth functionality implemented

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Traits\ApiResponse;
+use App\Http\Requests\ChnagePasswordRequest;
 use App\Http\Requests\LoginRequest;
 use App\Http\Resources\UserResource;
 use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
@@ -13,11 +15,11 @@ class AuthController extends Controller
     public function login(LoginRequest $request)
     {
         $data = $request->validated();
-        //check it the password match
-        $user = User::whee("email", $data["email"])->first();
+        // check it the password match
+        $user = User::where("email", $data["email"])->first();
 
         if (!$user || !Hash::check($data["password"], $user->password)) {
-            $this->error("", "Invalid credentials", 401);
+            return $this->error("", "Invalid credentials", 422);
         }
         // generate toekn for the user
         $token = $user->createToken("auth_token")->plainTextToken;
@@ -30,5 +32,36 @@ class AuthController extends Controller
             "Login successful",
             200,
         );
+    }
+
+    public function changePassword(ChnagePasswordRequest $request)
+    {
+        $data = $request->validated();
+        // check if the previous password is correct
+        $user = Auth::user();
+        if (!Hash::check($data["current_password"], $user->password)) {
+            return $this->error("", "Invalid credentials", 422);
+        }
+
+        // if so chnage the password
+        $user->password = Hash::make($data["new_password"]);
+        $user->save();
+        return $this->success(null, "Password changed successfully", 200);
+    }
+
+    public function me()
+    {
+        $user = Auth::user();
+        return $this->success(
+            new UserResource($user),
+            "User data fetched successfully",
+            200,
+        );
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+        return $this->success(null, "Logout successful", 200);
     }
 }
