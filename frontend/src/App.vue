e5506569656bd90eed2d20a41c4a37c42056e8ba
miraz <amiraliyimer@gmail.com>
2026-07-20 10:40:06 +0300

    setting up pinia and create axios instance

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,85 +1,55 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts" setup>
+import { ref } from 'vue';
+import { userAuthStore } from './stores/auth';
+
+const authStore = userAuthStore();
+
+const userData = ref({
+    email: '',
+    password: ''
+})
+
+const submitlogin = async () => {
+    authStore.login(userData.value.email, userData.value.password);
+}
+
 </script>
 
+
+
+
+
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <!-- //login form  with email and password -->
+    {{ console.log(authStore.user) }}
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+    <div class="flex flex-col items-center justify-center h-screen bg-gray-100">
+        <div class="w-full max-w-md">
+            <form @submit.prevent="submitlogin" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                <div class="mb-4">
+                    <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
+                        Email
+                    </label>
+                    <input
+                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="email" type="email" placeholder="Email" v-model="userData.email">
+                </div>
+                <div class="mb-6">
+                    <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+                        Password
+                    </label>
+                    <input
+                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                        id="password" type="password" placeholder="******************" v-model="userData.password">
+                </div>
+                <div class="flex items-center justify-between">
+                    <button
+                        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                        type="submit">
+                        Sign In
+                    </button>
+                </div>
+            </form>
+        </div>
     </div>
-  </header>
-
-  <RouterView />
-</template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+</template
---
+++ frontend/src/stores/auth.ts
@@ -0,0 +1,19 @@
+import * as authapi from '@/api/auth'
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+
+export const userAuthStore = defineStore('userAuth', () => {
+  const user = ref({})
+
+  const login = (email: string, password: string) => {
+    try {
+      const response = authapi.login(email, password)
+      console.log(response)
+      user.value = response
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  return { user, login }
+})
